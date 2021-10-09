c4dc65744831f955cf0a04dc4b45463c1f51c243
LightenJ <55630543+LightenJ@users.noreply.github.com>
2021-10-09 14:54:59 -0500

    Commit changes necessary to deploy to Google Cloud Platform
    Also includes the node_modules directory, which is a lot of extra storage space, but allows ng build to run.

--- angular_proto/jms-ping-pong-proto/src/environments/environment.ts
+++ angular_proto/jms-ping-pong-proto/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+     production: false,
+     webapiurl: (window as any)["envconfig"]["apiurl"] || "default"                   
 };
 
 /*
