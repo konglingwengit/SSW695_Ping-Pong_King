4a839f2559fbbdd5987a7152ba8c89f58435cec3
Agrawal-Deepti <78319350+Agrawal-Deepti@users.noreply.github.com>
2021-10-17 13:56:28 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,6 @@ from predictions import win_prediction, total_points_prediction, number_of_games
 from predictions import games_decided_by_extra_points_prediction, third_game_winner_prediction
 app = Flask(__name__)
 
-import asdfgh
-
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
