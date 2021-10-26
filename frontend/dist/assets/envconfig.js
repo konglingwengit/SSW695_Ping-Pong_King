ec36a3a35b103c00b12b7336082faaf0474ee636
LightenJ <55630543+LightenJ@users.noreply.github.com>
2021-10-25 20:46:20 -0500

    Update so that we don't need to do anything but "ng build" and commit.

--- frontend/dist/assets/envconfig.js
+++
@@ -1,7 +0,0 @@
-(function(window) {
-      window["envconfig"] = window["envconfig"] || {};
-
-      // Environment variables
-      //window["envconfig"]["apiurl"] = "https://ping-pong-king-666.uc.r.appspot.com/api";
-      window["envconfig"]["apiurl"] = "https://angular-deployment-328516.uc.r.appspot.com/api";
-})(this);
--- frontend/dist/assets/envconfig.template.js
+++
@@ -1,6 +0,0 @@
-(function(window) {
-      window.envconfig = window.envconfig || {};
-
-      // Environment variables
-      window["envconfig"]["apiurl"] = "${API_URL}";           
-})(this);
