ba02834431fb4ef8584e09c625181465fd18792f
Ehtisham Sabir <74449120+konglingwengit@users.noreply.github.com>
2021-10-30 02:46:48 +0500

    prediction_update

--- backend/ML/Predictor.py
+++ backend/ML/Predictor.py
@@ -11,26 +11,71 @@ def Predict(A_id, B_id):
     A_won = 0
     B_played = 0
     B_won = 0
+    A_win_rate = 0
+    B_win_rate = 0
     for i in data:
-        if A_id in [i["Player_A_ID"], i["Player_B_ID"]]:
-            if i["who_won"] == A_id:
-                A_won += 1
-            A_played += 1
-        if B_id in [i["Player_A_ID"], i["Player_B_ID"]]:
-            if i["who_won"] == B_id:
-                B_won += 1
-            B_played += 1
-    try:
-        A_win_rate = (A_played / A_won) * 100
-        B_win_rate = (B_played / B_won) * 100
+        if A_id == int(i["Player_A_ID"]):
+            for j in range(2,5):
+                setpointa = i[f'Set{str(j)}_A']
+                setpointb = i[f'Set{str(j)}_B']
+                if setpointa == '-' or setpointb == '-':
+                    break
+                if int(setpointa) > int(setpointb):
+                    A_won+=1
+                A_played+=1
+        elif A_id == int(i["Player_B_ID"]):
+            for j in range(2,5):
+                setpointa = i[f'Set{str(j)}_A']
+                setpointb = i[f'Set{str(j)}_B']
+                if setpointa == '-' or setpointb == '-':
+                    break
+                if int(setpointb) > int(setpointa):
+                    A_won+=1
+                A_played+=1
+
+        if B_id == int(i["Player_A_ID"]):
+            for j in range(2,5):
+                setpointa = i[f'Set{str(j)}_A']
+                setpointb = i[f'Set{str(j)}_B']
+                if setpointa == '-' or setpointb == '-':
+                    break
+                if int(setpointa) > int(setpointb):
+                    B_won+=1
+                B_played+=1
+        elif B_id == int(i["Player_B_ID"]):
+            for j in range(2,5):
+                setpointa = i[f'Set{str(j)}_A']
+                setpointb = i[f'Set{str(j)}_B']
+                if setpointa == '-' or setpointb == '-':
+                    break
+                if int(setpointb) > int(setpointa):
+                    B_won+=1
+                B_played+=1
+
+    if A_played == 0 and B_played == 0:
+        print('There is no record of these teams so,50% winning chances for each.')
+        return
+    elif A_played == 0:
+        A_win_rate = 0
+        try:
+            B_win_rate = (B_won/B_played) * 100
+        except ZeroDivisionError:
+            B_win_rate = 0
+        print(f'There is no record of team A ({A_id}) but team B`s winning rate is {"{:.2f}".format(B_win_rate)}%')
+
+    elif B_played == 0:
+        A_win_rate = (A_won/A_played) * 100
+        B_win_rate = 0
+        print(f'There is no record of team B ({B_id}) but team A`s winning rate is {"{:.2f}".format(A_win_rate)}%')
+        return
+    else:
+        A_win_rate = (A_won/A_played) * 100
+        B_win_rate = (B_won/B_played) * 100
 
         probability_A = (A_win_rate / (A_win_rate + B_win_rate)) * 100
         probability_B = (B_win_rate / (A_win_rate + B_win_rate)) * 100
-    except ZeroDivisionError:
-        probability_A = 50
-        probability_B = 50
 
-    print(f"Team A's({A_id}) winning chances are {probability_A}%, and B's({B_id}) chances are {probability_B}%")
+        print(f"Team A's({A_id}) winning chances are {probability_A}%, and B's({B_id}) chances are {probability_B}%")
 
 
 if __name__ == '__main__':
