bb9ee3085f5418ed969cac2cb63d20d3480aa1d8
konglingwengit <74449120+konglingwengit@users.noreply.github.com>
2021-11-09 17:30:57 -0500

    Update final_prediction_1109.py

--- backend/final_prediction_1109.py
+++ backend/final_prediction_1109.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -48,6 +42,7 @@ from pprint import pprint
 log_model = LogisticRegression(max_iter=131, verbose=2, random_state=42)
 log_model.fit(x_train, y_train)
 y_pred_log = log_model.predict(x_test)
+ans = (f'[{a}, 4,85, {b}, 1]')
 
 # # Classificcation by Random forest model
 rf = RandomForestClassifier(random_state=42)
@@ -62,6 +57,7 @@ model = GaussianNB()
 # Train the model using the training sets
 model.fit(x_train,y_train)
 y_pred = model.predict(x_test)
+
 #print(metrics.accuracy_score(y_test, y_pred))
 
 
@@ -90,10 +86,43 @@ log_reg.fit(trainX, trainY)
 y_predE = log_reg.predict(testX)
 
 
-if __name__ == '__main__':
-    a = input('playerA id : ')
-    b = input('playerB id : ')
-    found = False
+# if __name__ == '__main__':
+#     a = input('playerA id : ')
+#     b = input('playerB id : ')
+#     found = False
+#     for index, row in df.iterrows():
+#         if a == str(row["playerA"]) and b == str(row["PlayerB"]):
+
+#             if row['who_win'] == 0:
+#                 who_win = a
+#             else:
+#                 who_win = b
+
+#             if row["First Game Winner"] == 0:
+#                 first_win = a
+#             else:
+#                 first_win = b
+
+#             print(f'Prediction fields {who_win}, {row["Exact Number of Sets"]}, {row["Total Points"]}, '
+#                   f'{first_win}, {row["Sets Decided by Extra Points"]} ')
+
+#             found = True
+#             print('final result\n'
+#                   f'{who_win}, {row["Exact Number of Sets"]}, {row["Total Points"]}, '
+#                   f'{first_win}, {row["Sets Decided by Extra Points"]} ')
+
+#             break
+#     if not found:
+#         print('[0,4,85,0,1]'
+#               '\n'
+#               f'[{a}, 4,85, {b}, 1]')
+
+def predict(playerA_ID,playerB_ID):
+    status = False
+    a = str(playerA_ID)
+    b = str(playerB_ID)
+    ans = (f'[{a}, 4,85, {b}, 1]')
+           
     for index, row in df.iterrows():
         if a == str(row["playerA"]) and b == str(row["PlayerB"]):
 
@@ -107,23 +136,14 @@ if __name__ == '__main__':
             else:
                 first_win = b
 
-            print(f'Prediction fields {who_win}, {row["Exact Number of Sets"]}, {row["Total Points"]}, '
+            return(f'{who_win}, {row["Exact Number of Sets"]}, {row["Total Points"]}, '
                   f'{first_win}, {row["Sets Decided by Extra Points"]} ')
 
             found = True
-            print('final result\n'
+            return('final result\n'
                   f'{who_win}, {row["Exact Number of Sets"]}, {row["Total Points"]}, '
                   f'{first_win}, {row["Sets Decided by Extra Points"]} ')
 
             break
-    if not found:
-        print('[0,4,85,0,1]'
-              '\n'
-              f'[{a}, 4,85, {b}, 1]')
-
-
-# In[ ]:
-
-
-
-
+    if not status:
+        return ans
